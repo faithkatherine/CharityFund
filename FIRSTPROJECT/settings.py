ac84fd5f7af01ffd7de630e73051b0240fccaefe
faithkatherine <faithcathy12@gmail.com>
2020-12-14 15:53:01 +0300

    making changes

--- FIRSTPROJECT/settings.py
+++ FIRSTPROJECT/settings.py
@@ -10,7 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os,
+import os
 import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -141,5 +141,6 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/Media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Media_cdn')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 django_heroku.settings(locals())
