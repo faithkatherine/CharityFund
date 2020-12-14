cbb3e8fe2cb60921c2f7700090888e37d814932e
faithkatherine <faithcathy12@gmail.com>
2020-12-14 15:43:29 +0300

    my commit

--- FIRSTPROJECT/settings.py
+++ FIRSTPROJECT/settings.py
@@ -10,7 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
-import os
+import os,
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -141,3 +142,4 @@ MEDIA_URL = '/Media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Media_cdn')
 
+django_heroku.settings(locals())
